555640dcaeb021c53b938bc06eaec6e367ba95d6
Eddy <eddyukpong@gmail.com>
2023-03-07 21:07:48 +0100

    Bug fixes

--- src/Question.js
+++ src/Question.js
@@ -3,66 +3,66 @@ import he from "he"
 
 export default function Question(props) {
 
-     let mode = props.mode ? "darkmode" : "";
-    
+    let mode = props.mode ? "darkmode" : "";
+
     const answerElements = props.answers.map(opt => {
-        
+
         // Highlight selected options
         let styles;
-        if(!props.mode) {
+        if (!props.mode) {
             styles = {
                 backgroundColor: `${!opt.isHeld ? "#F5F7FB" : "#D6DBF5"}`,
-                border: "1px solid #4D5B9E" 
+                border: "1px solid #4D5B9E"
             }
         } else {
             styles = {
                 backgroundColor: `${!opt.isHeld ? "" : "#4D5B9E"}`,
-                color: "#F5F7FB" 
+                color: "#F5F7FB"
             }
         }
-        
+
         // Logic to highlight right/wrong answers
-        if(props.showAnswer && !props.mode) {
-            if(opt.isHeld && opt.value === props.correct) {
-                    styles = {backgroundColor: "#94D7A2", border: "none"}  
-            }
-             else if (opt.isHeld && opt.value !== props.correct) {
-                styles = {backgroundColor: "#F8BCBC", border: "none"}
-            } 
-            else if(opt.value === props.correct) {
-                styles = {backgroundColor: "#94D7A2", border: "none"}
-            }
-        } 
-        else 
-        if(props.showAnswer && props.mode) {
-            if(opt.isHeld && opt.value === props.correct) {
-                styles = {backgroundColor: "#19f348", border: "none"}  
+        if (props.showAnswer && !props.mode) {
+            if (opt.isHeld && opt.value === props.correct) {
+                styles = { backgroundColor: "#94D7A2", border: "none" }
             }
             else if (opt.isHeld && opt.value !== props.correct) {
-                styles = {backgroundColor: "#e92121", border: "none"}
-            } 
-            else if(opt.value === props.correct) {
-                styles = {backgroundColor: "#19f348", border: "none"}
+                styles = { backgroundColor: "#F8BCBC", border: "none" }
+            }
+            else if (opt.value === props.correct) {
+                styles = { backgroundColor: "#94D7A2", border: "none" }
             }
         }
-        
+        else
+            if (props.showAnswer && props.mode) {
+                if (opt.isHeld && opt.value === props.correct) {
+                    styles = { backgroundColor: "#59dd76", border: "none" }
+                }
+                else if (opt.isHeld && opt.value !== props.correct) {
+                    styles = { backgroundColor: "#e92121", border: "none" }
+                }
+                else if (opt.value === props.correct) {
+                    styles = { backgroundColor: "#59dd76", border: "none" }
+                }
+            }
+
         return (
-            <button key= {opt.id} 
-                onClick= {() => props.selected(opt.id, props.id)}
-                className = {`options btn ${mode}`}
-                style = {styles}
+            <button key={opt.id}
+                onClick={() => props.selected(opt.id, props.id)}
+                className={`options btn ${mode}`}
+                style={styles}
             >
-                 {he.decode(opt.value)}
+                {he.decode(opt.value)}
             </button>
         )
     })
     return (
-        <div className = "main-container">
-            <h3 key= {props.id}>{he.decode(props.questions)}</h3>
-            <div className= "option-case">
+        <div className="main-container">
+            <h3 key={props.id}>{he.decode(props.questions)}</h3>
+            <div className="option-case">
                 {answerElements}
             </div>
-            <hr/>
+            <hr />
         </div>
     )
 }
